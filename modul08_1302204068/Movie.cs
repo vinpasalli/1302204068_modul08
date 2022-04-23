c9fe387273bfc7288adf0eb4159d1d67c76d62ee
vin14 <pasallimelvin@student.telkomuniversity.ac.id>
2022-04-23 12:16:58 +0700

    menambahkan API movies

---
+++ modul08_1302204068/Movie.cs
@@ -0,0 +1,20 @@
+﻿namespace modul08_1302204068
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get; set; }
+
+        public List<string> Stars { get; set; }
+
+        public string Description { get; set; }
+
+        public Movie(string judul, string direktor, List<string> bintang, string deskripsi)
+        {
+            this.Title = judul;
+            this.Director = direktor;
+            this.Stars = bintang;
+            this.Description = deskripsi;
+        }
+    }
+}
---
+++ modul08_1302204068/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul08_1302204068
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        public static List <string> bintang1 = new List<string>
+        {
+            "Tim Robbins", "Morgan Freeman", "Bob Guntom", "William Sandler"
+        }
+
+        public static List<string> bintang1 = new List<string>
+        {
+            "Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton"
+        }
+
+        // POST api/<MovieController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MovieController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MovieController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
